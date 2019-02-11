98eedc4bdbb3bfcb7ba8f907fd75cfb31260627a
kernel32 <sergii.rubinov@gmail.com>
2019-02-12 01:26:58 +0200

    user

--- src/test/java/ua/in/javac/entity/manytomany/HibernateBookRepositoryTest.java
+++ src/test/java/ua/in/javac/entity/manytomany/HibernateBookRepositoryTest.java
@@ -23,6 +23,5 @@ public class HibernateBookRepositoryTest {
     @Test
     public void defaultFetchModeHql() throws Exception {
         Assert.assertEquals("user" == "user", true);
-        System.out.println("user");
     }
 }
