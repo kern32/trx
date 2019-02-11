8d9e33f4d20b7a40ddc541767fd8fa36b3353ace
kernel32 <sergii.rubinov@gmail.com>
2019-02-12 00:59:43 +0200

    kernel32

--- src/test/java/ua/in/javac/entity/manytomany/HibernateBookRepositoryTest.java
+++ src/test/java/ua/in/javac/entity/manytomany/HibernateBookRepositoryTest.java
@@ -22,7 +22,7 @@ public class HibernateBookRepositoryTest {
 
     @Test
     public void defaultFetchModeHql() throws Exception {
-        Assert.assertEquals("kernel32" == "kernel32", true);
-        System.out.println("kernel32");
+        Assert.assertEquals("user" == "user", true);
+        System.out.println("user");
     }
 }
