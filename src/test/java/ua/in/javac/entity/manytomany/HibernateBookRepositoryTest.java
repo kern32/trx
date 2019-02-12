81c64e6e4c4abc6b05d922457e8459e200edf8cd
kernel32 <sergii.rubinov@gmail.com>
2019-02-12 22:51:25 +0200

    k 3

--- src/test/java/ua/in/javac/entity/manytomany/HibernateBookRepositoryTest.java
+++ src/test/java/ua/in/javac/entity/manytomany/HibernateBookRepositoryTest.java
@@ -23,6 +23,6 @@ public class HibernateBookRepositoryTest {
     @Test
     public void defaultFetchModeHql() throws Exception {
         Assert.assertEquals("k" == "k", true);
-        System.out.println(2);
+        System.out.println(3);
     }
 }
